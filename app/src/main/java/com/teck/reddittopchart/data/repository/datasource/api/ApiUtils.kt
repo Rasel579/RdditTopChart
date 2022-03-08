4da0f0586968bab17e69fd44d77eb9f9517a1273
Rasel579 <Rasel579@gmail.com>
2022-03-08 20:24:00 +0500

    1. Added entities, retrofit data impl, Repository, Paging 3.0 library, Di koin

---
+++ app/src/main/java/com/teck/reddittopchart/data/repository/datasource/api/ApiUtils.kt
@@ -0,0 +1,18 @@
+package com.teck.reddittopchart.data.repository.datasource.api
+
+import com.google.gson.Gson
+import okhttp3.OkHttpClient
+
+object ApiUtils {
+    fun getOkHttpClientBuilder(): OkHttpClient {
+        val httpClient = OkHttpClient.Builder()
+            .addInterceptor { chain ->
+                val original = chain.request()
+                val request = original.newBuilder()
+                    .method(original.method, original.body)
+                    .build()
+                chain.proceed(request)
+            }
+        return httpClient.build()
+    }
+}
